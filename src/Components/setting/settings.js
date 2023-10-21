c9b56691bca5d33249e3b1150a85e6267a945aa1
Tarık Tunç <tarktunc@hotmail.com>
2023-10-21 21:59:44 +0300

    Setting Page update

--- src/Components/setting/settings.js
+++ src/Components/setting/settings.js
@@ -1,5 +1,15 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 
-export default function Settings() {
-  return <div>Settings</div>;
+export default function Settings({ handleClick }) {
+  return (
+    <div className="absolute bottom-10 right-10  w-24 h-24 rounded-full">
+      <button
+        onClick={handleClick}
+        type="button"
+        className="bg-slate-700 w-24 h-24 rounded-full text-white text-lg text">
+        Settings
+      </button>
+    </div>
+  );
 }
--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,99 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 import YoutubeChannels from "../Components/youtubeChannels/youtubeChannels.js";
 import Settings from "../Components/setting/settings.js";
 
 export default function Home() {
+  const [viewSettings, setViewSettings] = useState(true);
+  const [channelUrl, setChannelUrl] = useState(4);
+  const [channelBtn, setChannelBtn] = useState([4, 6, 8, 9, 12]);
+  const [channelIdInput, setChannelIdInput] = useState(
+    Array(channelUrl)
+      .fill("")
+      .map((item, index) => ({ id: index, value: "" }))
+  );
+
+  const handleClick = () => {
+    setViewSettings(!viewSettings);
+  };
+
+  const formHandle = (e) => {
+    e.preventDefault();
+    console.log(channelIdInput);
+  };
+
+  const handleInputChange = (id, value) => {
+    const newChannelIdInput = [...channelIdInput];
+    newChannelIdInput[id] = { id: id, value: value };
+    setChannelIdInput(newChannelIdInput);
+  };
+
+  const channelInput = () => {
+    let channels = [];
+    for (let i = 0; i < channelUrl; i++) {
+      channels.push(
+        <div key={`channelNames${i}`} className="flex m-1 p-1 ">
+          <p className="mr-4 text-white">Channel Name</p>
+          <input
+            className="text-black"
+            key={`channelName${i}`}
+            type="text"
+            value={channelIdInput[i] ? channelIdInput[i].value : ""}
+            onChange={(e) => handleInputChange(i, e.target.value)}
+          />
+        </div>
+      );
+    }
+    return channels;
+  };
+  const channelButtons = () => {
+    let buttons = [];
+    for (let i = 0; i < channelBtn.length; i++) {
+      buttons.push(
+        <button
+          className="w-10 m-1 p-2 rounded-md bg-slate-950"
+          id={`btn${channelBtn[i]}`}
+          key={`btnKey${channelBtn[i]}`}
+          onClick={() => setChannelUrl(channelBtn[i])}>
+          {channelBtn[i]}
+        </button>
+      );
+    }
+    return buttons;
+  };
+
   return (
-    <div>
-      <YoutubeChannels />
-      <Settings />
+    <div className="h-screen">
+      {viewSettings ? (
+        <div className="bg-slate-700  p-5 mb-20 mr-10 absolute right-0 bottom-0 text-center rounded-md">
+          {/* Channel Buttons */}
+          <div className="flex justify-around items-center text-white">
+            {channelButtons()}
+          </div>
+          {/* Channel Form */}
+          <form onSubmit={formHandle}>
+            {channelInput()}
+            <button
+              type="submit"
+              className="w-full m-1 p-2 rounded-md bg-slate-950 text-white">
+              Submit
+            </button>
+          </form>
+          <button
+            onClick={handleClick}
+            className="w-full m-1 p-2 rounded-md bg-slate-950 text-white">
+            EXIT
+          </button>
+        </div>
+      ) : (
+        <div>
+          <Settings handleClick={handleClick} />
+        </div>
+      )}
+
+      <div>
+        <YoutubeChannels displayCount="4" />
+      </div>
     </div>
   );
 }
