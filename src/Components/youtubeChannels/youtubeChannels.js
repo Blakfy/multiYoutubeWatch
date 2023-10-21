0c84b158b8fc0118a25f0d8fd5b47299c8e7f884
Henkerr <118183620+Henkerr@users.noreply.github.com>
2023-10-21 22:04:40 +0300

    YoutubeChannels.js update

--- src/Components/youtubeChannels/youtubeChannels.js
+++ src/Components/youtubeChannels/youtubeChannels.js
@@ -11,15 +11,11 @@ export default function YoutubeChannels() {
   return (
     <div className="grid grid-cols-3 sm:grid-cols-3 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-2 gap-0">
       {videoLink.map((videoId, i) => (
-        <div
-          key={i}
-          className="text-center p-0"
-          style={{ marginBottom: "0px" }}>
+        <div key={i} className="text-center p-0">
           <div className="relative" style={{ paddingBottom: "56.25%" }}>
             <iframe
               className="absolute inset-0 w-full h-full"
               src={`https://www.youtube.com/embed/${videoLink[i]}?autoplay=1&mute=1`}
-              frameBorder="0"
               allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
               allowFullScreen
               title={`YouTube Video ${i}`}
