b051ce578bc844ed025db8319aa3804aadeb320e
Henkerr <118183620+Henkerr@users.noreply.github.com>
2023-12-14 18:06:42 +0300

    Update youtubeChannels.js

--- src/Components/youtubeChannels/youtubeChannels.js
+++ src/Components/youtubeChannels/youtubeChannels.js
@@ -6,13 +6,13 @@ export default function YoutubeChannels({ channelKey }) {
 
   let gridClass;
   if (numVideos === 1) {
-    gridClass = "grid-cols-1";
+    gridClass = "grid-cols-1 h-screen w-base";
   } else if (numVideos === 4) {
-    gridClass = "grid-cols-2";
+    gridClass = "grid-cols-2 h-screen w-base";
   } else if (numVideos === 9) {
-    gridClass = "grid-cols-3";
+    gridClass = "grid-cols-3 h-screen w-base";
   } else if (numVideos === 16) {
-    gridClass = "grid-cols-4";
+    gridClass = "grid-cols-4 h-screen w-base";
   } else {
     gridClass = "grid-cols-1 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-4 xl:grid-cols-2";
   }
@@ -21,7 +21,7 @@ export default function YoutubeChannels({ channelKey }) {
     <div className={`grid ${gridClass} gap-0`}>
       {channelKey.slice(0, numVideos).map((videoId, i) => (
         <div key={i} className="text-center p-0">
-          <div className="relative" style={{ paddingBottom: "56.25%" }}>
+          <div className="relative" style={{ paddingBottom: "50%" }}>
             <iframe
               className="absolute inset-0 w-full h-full"
               src={`https://www.youtube.com/embed/${videoId}?autoplay=1&mute=1`}
